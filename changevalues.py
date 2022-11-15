59d2eadf7943ee68d384824af313b54607347a78
rishi7822 <82447941+rishi7822@users.noreply.github.com>
2022-11-16 00:33:40 +0530

    Add files via upload

---
+++ changevalues.py
@@ -0,0 +1,12 @@
+#changes values in the list
+#we can change values at some particular index in the list
+
+week = ["sunday","monday","tuesday","wednesday","thursday"]
+print(week)
+week[0]="england"
+print(week)
+
+cricket = ["pujara","dravid","ponting","hayden"]
+print(cricket)
+cricket[3]="katich"
+print(cricket)
